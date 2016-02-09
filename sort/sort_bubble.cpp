92b0c56d3e4f713d150884c2fe3ec71b90b929c5
luoxn28 <luoxn28@163.com>
2016-02-09 22:54:32 +0800

    修改了冒泡排序算法

--- sort/sort_bubble.cpp
+++ sort/sort_bubble.cpp
@@ -39,12 +39,12 @@ int main(void)
 */
 void bubble_sort(int arr[], int length)
 {
-	for (int i = 0; i < length -1; i++)
+	for (int i = 0; i < length; i++)
 	{
-		for (int j = i + 1; j < length; j++)
+		for (int j = 0; j < length - i - 1; j++)
 		{
-			if (arr[i] > arr[j])
-				swap(arr[i], arr[j]);
+			if (arr[j] > arr[j+1])
+				swap(arr[j], arr[j+1]);
 		}
 	}
 }
